06500cc7e585bae49efa3754e2fe9adf98c73b67
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-08-12 18:56:50 -0500

    added JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-const secondClock = document.querySelector("#seconds-clock");
-const minuteHand = document.querySelector("#minutes-hand");
-const hourHand = document.querySelector("#hours-hand");
+const secondClock = document.getElementById('seconds-clock')
+const minuteHand = document.getElementById('minutes-hand')
+const hourHand = document.getElementById('hours-hand')
 
 function getTime () {
 
     const now = new Date () 
     const seconds = now.getSeconds()
-    const minute = now.getMinutes()
-    const hour = now.getHours()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
 
-    
+    const timeInterval = 6
+
+    secondClock.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'
+    minuteHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)'
+    hourHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)' 
 }
+
+setInterval(getTime, 100)
 
