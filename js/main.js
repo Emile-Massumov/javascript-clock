46c5418e0a9ed0d8d195d9b73eae1e9da1c3ada6
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-08-12 18:39:18 -0500

    added hour minutes in javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const secondClock = document.querySelector("#seconds-clock");
+const minuteHand = document.querySelector("#minutes-hand");
+const hourHand = document.querySelector("#hours-hand");
+
+function getTime () {
+
+    const now = new Date () 
+    const seconds = now.getSeconds()
+    const minute = now.getMinutes()
+    const hour = now.getHours()
+
+    
+}
+
